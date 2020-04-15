b489f2d498ad1f18f154e42a4a9d12e23fa00bf6
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-04-15 04:36:42 -0300

    removed unused files

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>test</h1>
+      </div>
+    )
+  }
+}
