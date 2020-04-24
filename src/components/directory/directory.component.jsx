a67cd6c6ff770c3ef05303007f8330d34ad364f8
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2020-04-24 04:29:13 -0300

    preparando componente directorio para mostrar proyectos

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,2 +1,81 @@
-import React from 'react'
+import React from "react";
 
+import MenuItem from "../menu-item/menu-item.component";
+
+import "./directory.styles.scss";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      section: [
+        {
+          title: "",
+          imageUrl: "https://images.pexels.com/photos/984619/pexels-photo-984619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          id: 1,
+          linkUrl: "hats",
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          linkUrl: "",
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://images.pexels.com/photos/2065695/pexels-photo-2065695.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          id: 3,
+          linkUrl: "",
+        },
+        {
+          title: "womens",
+          imageUrl: "https://images.pexels.com/photos/2906664/pexels-photo-2906664.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          size: "large",
+          id: 4,
+          linkUrl: "",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://images.pexels.com/photos/842811/pexels-photo-842811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          size: "large",
+          id: 5,
+          linkUrl: "",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.section.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
+
+/*
+It´s the same
+
+      <div className="directory-menu">
+        {this.state.section.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+
+      <div className="directory-menu">
+        {this.state.section.map(({ title, imageUrl, id, size, history }) => (
+            <MenuItem 
+              key={id} 
+              title={title} 
+              imageUrl={imageUrl} 
+              size={size} 
+              history={history} 
+            />
+          ))}
+      </div>
+ */
