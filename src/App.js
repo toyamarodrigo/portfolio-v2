9fdc8918c7eb1834d2bd35bce1a2db08beed9920
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2020-04-24 21:21:45 -0300

    borrado algunas carpetas, se hacia muy complicado. empecé del header(navbar) agregue react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Header from './components/header/header.component'
-import HomePage from "./pages/homepage/homepage.component";
-import AboutPage from "./pages/aboutpage/aboutpage.component";
+// import { Container } from 'react-bootstrap';
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
+import AboutPage from './pages/aboutpage/aboutpage.component';
 
-function App() {
-  return (
-    <div>
-    <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/about" component={AboutPage} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/about" component={AboutPage} />
+          </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,22 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/logo.svg";
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+// import { ReactComponent as Logo } from '../../assets/logo.svg';
 
-import "./header.styles.scss";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import './header.styles.scss';
 
 const Header = () => (
   <div className="header">
-    <Link to="/" className="logo-container">
-      <Logo className="logo" />
-    </Link>
-    <div className='options'>
-      <Link to='/about' className='option'>
-        ABOUT
-      </Link>
-      <Link to='/contact' className='option'>
-        CONTACT
-      </Link>
-    </div>
+    <Navbar bg="light" collapseOnSelect id="navbar">
+      <Container>
+          <Navbar.Brand>
+            <Nav.Link className="navbar-brand" to="/">
+              Website
+            </Nav.Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto justify-content-between">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+              <Nav.Link href="#Contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Container>
+    </Navbar>
   </div>
 );
 
