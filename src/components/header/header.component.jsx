cc8d17c5af823846581beab06ba3fdc9ea0d55bf
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2020-04-28 05:17:34 -0300

    agregado hamburger menu bootstrap slide animation  from right to left, falta ease-out

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,11 +12,11 @@ const Header = () => (
       data-target="#navbarResponsive"
       className="justify-content-center"
     >
-      <Navbar bg="transparent" className="fixed-top" expand="lg">
+      <Navbar bg="transparent" variant="dark" className="fixed-top" expand="lg">
         <Container>
           <Navbar.Brand href="/">rt.codes</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
               <div className="link-items">
                 <LinkContainer to="/">
