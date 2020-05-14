71e753974263dbf11a158a7c5be79d3e2fedb99f
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2020-05-14 20:23:30 -0300

    Updates

---
+++ precache-manifest.e122c20822404c7fa1b0cc6040d35a30.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fb73f6d7f9a9b3baff66d3f28971ec",
+    "url": "/toyamarodrigo-v2.github.io/index.html"
+  },
+  {
+    "revision": "11897679125b2301cf56",
+    "url": "/toyamarodrigo-v2.github.io/static/css/2.d0a29668.chunk.css"
+  },
+  {
+    "revision": "8e45a32885849e98a03f",
+    "url": "/toyamarodrigo-v2.github.io/static/css/main.a35350d6.chunk.css"
+  },
+  {
+    "revision": "11897679125b2301cf56",
+    "url": "/toyamarodrigo-v2.github.io/static/js/2.e65ebe6d.chunk.js"
+  },
+  {
+    "revision": "364d48ca88b967e2bad347890666dd46",
+    "url": "/toyamarodrigo-v2.github.io/static/js/2.e65ebe6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e45a32885849e98a03f",
+    "url": "/toyamarodrigo-v2.github.io/static/js/main.2d45ca32.chunk.js"
+  },
+  {
+    "revision": "4d397d5c8f3ae1b32005",
+    "url": "/toyamarodrigo-v2.github.io/static/js/runtime-main.f09fc5a6.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/toyamarodrigo-v2.github.io/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/toyamarodrigo-v2.github.io/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "487556fea5945ce8aed5d3b5568d07a9",
+    "url": "/toyamarodrigo-v2.github.io/static/media/avatar.487556fe.jpeg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/toyamarodrigo-v2.github.io/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "46e822d32c99b0e26f5af51ded18009b",
+    "url": "/toyamarodrigo-v2.github.io/static/media/project-image1.46e822d3.jpg"
+  }
+]);
