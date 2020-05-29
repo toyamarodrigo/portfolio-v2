7dc91d42e499f678f7c5a5cf273f9aaa082f0364
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2020-05-29 16:06:40 -0300

    trying browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, HashRouter as Router, Route } from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import profile from './profile.json';
 
